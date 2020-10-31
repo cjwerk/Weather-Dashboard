c0e48856360496e29ee5e84130995df9d39c2953
Chris Werkheiser <cjwerk@gmail.com>
2020-10-30 21:27:45 -0700

    not there yet

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,15 +1,25 @@
-var button = document.querySelector('button')
-var inputValue = document.querySelector('.inputValue')
+var button = document.querySelector('button');
+var inputValue = document.querySelector('.inputValue');
 var name = document.querySelector('.name');
 var desc = document.querySelector('.desc');
 var temp = document.querySelector('.temp');
 
-button.addEventListener('click', function() {
 
-fetch("https://api.openweathermap.org/data/2.5/forecast?q="+input.Value+"&appid=60ce7466ebde7b0aecd67dbb4864a061")
 
-.then(response => response.json())
-.then(data => console.log(data))
-
-.catch(error => alert("Wrong city name!"))
-})
+button.addEventListener('click', function(name){
+    fetch("https://api.openweathermap.org/data/2.5/forecast?q="+input.Value+"&appid=60ce7466ebde7b0aecd67dbb4864a061")
+    .then(response => response.json())
+    .then(data => {
+      var tempValue = data['main']['temp'];
+      var nameValue = data['name'];
+      var descValue = data['weather'][0]['description'];
+    
+      main.innerHTML = nameValue;
+      desc.innerHTML = "Desc - "+descValue;
+      temp.innerHTML = "Temp - "+tempValue;
+      input.Value ="";
+    
+    })
+    
+    .catch(error => alert("Wrong city name!"));
+    })
